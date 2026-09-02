4ddb400c5d24039c3c796a01b0e0742b237fb5d6
chkyul95 <294778375+chkyul95@users.noreply.github.com>
2026-09-02 22:40:21 +0900

    feat: add function to disable system animations
    - Introduce SetSystemAnimations() and invoke it at main startup to disable Windows animations, achieving instant virtual desktop transitions.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <windows.h>
 #include <iostream>
 
+void SetSystemAnimations(bool enable) {
+	SystemParametersInfoW(
+		SPI_SETCLIENTAREAANIMATION,
+		0,
+		(PVOID)(uintptr_t)(enable ? TRUE : FALSE),
+		SPIF_UPDATEINIFILE | SPIF_SENDCHANGE
+	);
+	
+	ANIMATIONINFO ai = {};
+	ai.cbSize = sizeof(ANIMATIONINFO);
+	ai.iMinAnimate = enable ? 1 : 0;
+	
+	SystemParametersInfoW(
+		SPI_SETANIMATION,
+		sizeof(ANIMATIONINFO),
+		&ai,
+		SPIF_UPDATEINIFILE | SPIF_SENDCHANGE
+	);
+}
+
 void SwitchVirtualDesktop(bool goRight) {
 	INPUT releaseAlt = {};
 	releaseAlt.type = INPUT_KEYBOARD;
@@ -36,6 +56,9 @@ void SwitchVirtualDesktop(bool goRight) {
 }
 
 int main() {
+	
+	SetSystemAnimations(false);
+	
 	if (!RegisterHotKey(NULL, 1, MOD_ALT | MOD_NOREPEAT, '1') || !RegisterHotKey(NULL, 2, MOD_ALT | MOD_NOREPEAT, '2')) {
 		std::cerr << "Failed to register hot key!" << std::endl;
 		return 1;
