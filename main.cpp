57bf71b73315d7142cc3ce5c676d35c396896dcf
chkyul95 <294778375+chkyul95@users.noreply.github.com>
2026-09-02 23:47:36 +0900

    feat: add GUI window with animation toggle option
    - Refactored console application to Win32 GUI window (`wWinMain`, `WndProc`)
    - Added checkbox to dynamically enable/disable Windows switching animations
    - Displays hotkey guide labels directly within the window interface

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <windows.h>
-#include <iostream>
+
+constexpr int IDC_CHK_ANIMATION = 2001;
 
 void SetSystemAnimations(bool enable) {
 	SystemParametersInfoW(
@@ -55,29 +56,80 @@ void SwitchVirtualDesktop(bool goRight) {
 	SendInput(6, inputs, sizeof(INPUT));
 }
 
-int main() {
-	
-	SetSystemAnimations(false);
-	
-	if (!RegisterHotKey(NULL, 1, MOD_ALT | MOD_NOREPEAT, '1') || !RegisterHotKey(NULL, 2, MOD_ALT | MOD_NOREPEAT, '2')) {
-		std::cerr << "Failed to register hot key!" << std::endl;
-		return 1;
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+	switch (msg) {
+		case WM_CREATE: {
+			CreateWindowW(
+				L"BUTTON", L"Turn off screen switching animation (Recommended)",
+				WS_VISIBLE | WS_CHILD | BS_AUTOCHECKBOX,
+				20, 20, 360, 24, hwnd, (HMENU)IDC_CHK_ANIMATION, NULL, NULL
+			);
+			CheckDlgButton(hwnd, IDC_CHK_ANIMATION, BST_CHECKED);
+			
+			CreateWindowW(
+				L"STATIC", L"- Alt + 1: Previous desktop\n- Alt + 2: Next desktop",
+				WS_VISIBLE | WS_CHILD | SS_LEFT,
+				20, 60, 280, 50, hwnd, NULL, NULL, NULL
+			);
+			
+			SetSystemAnimations(false);
+			
+			if (!RegisterHotKey(hwnd, 1, MOD_ALT | MOD_NOREPEAT, '1') || !RegisterHotKey(hwnd, 2, MOD_ALT | MOD_NOREPEAT, '2')) {
+				MessageBoxW(hwnd, L"Failed to register hotkey!", L"Error", MB_ICONERROR);
+			}
+			
+			return 0;
+		}
+		case WM_COMMAND: {
+			int id = LOWORD(wParam);
+			if (id == IDC_CHK_ANIMATION) {
+				bool checked = (IsDlgButtonChecked(hwnd, IDC_CHK_ANIMATION) == BST_CHECKED);
+				SetSystemAnimations(!checked);
+			}
+			return 0;
+		}
+		case WM_HOTKEY: {
+			if (wParam == 1) SwitchVirtualDesktop(false);
+			if (wParam == 2) SwitchVirtualDesktop(true);
+			return 0;
+		}
+		case WM_DESTROY:
+			UnregisterHotKey(hwnd, 1);
+			UnregisterHotKey(hwnd, 2);
+			PostQuitMessage(0);
+			return 0;
 	}
+	return DefWindowProcW(hwnd, msg, wParam, lParam);
+}
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow) {
+	const wchar_t CLASS_NAME[] = L"StealthDesktopSwitchClass";
 	
-	std::cout << "Virtual Desktop Switcher Running... (Alt + 1: Previous, Alt + 2: Next, Ctrl + C: Exit)" << std::endl;
+	WNDCLASSW wc = {};
+	wc.lpfnWndProc = WndProc;
+	wc.hInstance = hInstance;
+	wc.lpszClassName = CLASS_NAME;
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	
+	RegisterClassW(&wc);
+	
+	HWND hwnd = CreateWindowExW(
+		WS_EX_DLGMODALFRAME,
+		CLASS_NAME, L"Stealth Desktop Switcher",
+		WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,
+		CW_USEDEFAULT, CW_USEDEFAULT, 420, 180,
+		NULL, NULL, hInstance, NULL
+	);
+	
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
 	
 	MSG msg = {};
-	while (GetMessage(&msg, NULL, 0, 0)) {
-		if (msg.message == WM_HOTKEY) {
-			if (msg.wParam == 1) {
-				SwitchVirtualDesktop(false);
-			} else if (msg.wParam == 2) {
-				SwitchVirtualDesktop(true);
-			}
-		}
+	while (GetMessageW(&msg, NULL, 0, 0)) {
+		TranslateMessage(&msg);
+		DispatchMessageW(&msg);
 	}
-	
-	UnregisterHotKey(NULL, 1);
-	UnregisterHotKey(NULL, 2);
 	return 0;
 }
+
